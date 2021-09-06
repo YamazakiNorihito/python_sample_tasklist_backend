09c1489a715b6e86318a373200577338805f1d3c
山崎紀仁 <yamazaki@nikkeihr.co.jp>
2021-09-06 10:50:21 +0900

    settings url ALLOWED_HOSTS

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nextjs-tasklist-api.herokuapp.com']
 
 
 # Application definition
